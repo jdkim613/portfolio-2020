20ef9f8ecf4147523d6fc151ffab7d3df38520b0
John Kim <jj61397@gmail.com>
2020-04-19 19:57:04 -0700

    Nav Bar + Img Interaction

--- portfolio_2019/action.js
+++ portfolio_2019/action.js
@@ -23,13 +23,22 @@ $(document).ready(function() {
         }
       });
 
-    // Changing Images 1
-    $("#image-item-switch-0").click(function() {
-      $("#image-item-0").attr("src", "https://dynamics365cdn.azureedge.net/cvt-df1a53f60e0c95ad8766638639a309f77f978efd9f3526a9196ad0f4fbc0b652/pictures/pages/mixed-reality/guides/Panel2_2880x1200.jpg");
-      console.log("switching to image 0");
-    });
-    $("#image-item-switch-1").click(function() {
-      $("#image-item-0").attr("src", "https://dynamics365cdn.azureedge.net/cvt-9ef69ce73be714a74b9e06cc74cf0f693cefeaa446678982fc8a93dc3d767c38/pictures/pages/mixed-reality/remote-assist/hero_1826x1026.jpg");
-      console.log("switching to image 1");
-    });
-});
+      // Changing Images 1
+      $("#image-item-switch-0").click(function() {
+        $("#image-item-0").attr("src", "https://dynamics365cdn.azureedge.net/cvt-df1a53f60e0c95ad8766638639a309f77f978efd9f3526a9196ad0f4fbc0b652/pictures/pages/mixed-reality/guides/Panel2_2880x1200.jpg");
+        console.log("switching to image 0");
+      });
+      $("#image-item-switch-1").click(function() {
+        $("#image-item-0").attr("src", "https://dynamics365cdn.azureedge.net/cvt-9ef69ce73be714a74b9e06cc74cf0f693cefeaa446678982fc8a93dc3d767c38/pictures/pages/mixed-reality/remote-assist/hero_1826x1026.jpg");
+        console.log("switching to image 1");
+      });
+
+      // Add img Shawdow
+      $( ".case-item img" ).hover(
+        function() {
+          $(this).addClass( "image-shadow" );
+        }, function() {
+          $(this).removeClass( "image-shadow" );
+        });
+
+  });
